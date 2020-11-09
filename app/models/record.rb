d55a86ad66a8accfb81a5aa09a39fd7e74d392bb
Puneet Gupta <puneet@gmail.com>
2020-11-09 15:49:03 +0400

    Added Backend URL validation and callback to generate shorten link

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,10 @@
 class Record < ApplicationRecord
+
+  validates :url, format: URI::regexp(%w[http https])
+
+  before_create :set_shorten_url
+
+  def set_shorten_url
+    self.shorten_url = SecureRandom.alphanumeric(8)
+  end
 end
